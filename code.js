96763b339c2ea1517001f8e4ff6b5984000be22d
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-03-03 17:07:40 +0200

    added code generation to console when pressing buttons

---
+++ code.js
@@ -0,0 +1,6 @@
+console.log("AAAAAAAAAAAAAGH");
+$( document ).keydown( function (event) { 
+	console.log("a");
+	}
+);
+
--- scripts/code.js
+++ scripts/code.js
@@ -1,1 +1,20 @@
+var codeString = generateCode();
+var startIndex = 0;
+var endIndex = 7;
+
 console.log("AAAAAAAAAAAAAGH");
+$( document ).keydown( function (event) { 
+		console.log(codeString.substring(startIndex, endIndex));
+		startIndex += 7;
+		endIndex += 7;
+	}
+);
+
+function generateCode(){
+	var array = ["public ", "static ", "int ", "{ \n", "\n}\n", "return ", "int"];	
+	var string = "";
+	for(var i = 0; i < 1000; i++){
+		string += array[Math.floor((Math.random()*array.length))];
+	}
+	return string;
+}
