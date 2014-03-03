84de4f3f5d736b35c8ea6e35b5f9b3b281fd18c9
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-03-03 15:52:32 +0200

    clickable thing, better jquery

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var app = require('express')()
+var express = require('express')
+  , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
@@ -10,6 +13,13 @@ io.sockets.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
+  });
+  socket.on('button click', function (data) {
+    console.log("Tööt, received: %j", data);
+  });
+
+  socket.on('close', function() {
+    console.log('a client closed');
   });
 });
 
