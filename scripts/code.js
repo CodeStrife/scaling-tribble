f2b696ba1de389428311db63b4dc4f960ca0e6b1
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-03-03 17:56:53 +0200

    adding generated code to code div

--- scripts/code.js
+++ scripts/code.js
@@ -4,14 +4,23 @@ var endIndex = 7;
 
 console.log("AAAAAAAAAAAAAGH");
 $( document ).keydown( function (event) { 
-		console.log(codeString.substring(startIndex, endIndex));
+		var substring = codeString.substring(startIndex, endIndex);
+		var stringWithLinebreaks = "";
+		for(var i = 0; i < substring.length; i++){
+			if(substring[i] == '¤'){
+				stringWithLinebreaks += "<br/>";
+			} else {
+				stringWithLinebreaks += substring[i];
+			}
+		}
+		$('#code').append(stringWithLinebreaks);
 		startIndex += 7;
 		endIndex += 7;
 	}
 );
 
 function generateCode(){
-	var array = ["public ", "static ", "int ", "{ \n", "\n}\n", "return ", "int"];	
+	var array = ["public ", "static ", "int ", "{ ¤", "¤}¤", "return ", "double " , "String ", "void "];
 	var string = "";
 	for(var i = 0; i < 1000; i++){
 		string += array[Math.floor((Math.random()*array.length))];
