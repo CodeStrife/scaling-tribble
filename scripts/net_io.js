368a539d885088b2edde2b6d8079849357a4c004
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-03-06 10:04:34 -0500

    replaced the old code and net_io with new ones

--- scripts/net_io.js
+++ scripts/net_io.js
@@ -3,25 +3,44 @@
  *  All client-server functionality goes here.
  *
  */
-
+ 
+ var game = new Game("C");
+ var addCode = game.makeCode;
+ var playerCode = game.playerCode;
+ var codeGenIntervalID;
+ 
   //    INIT: Socket
   var socket = io.connect(window.location.hostname);
  
   //    BUTTON: Autocomplete
   $("#autocomplete").click( function() {
     console.log('Bought autocomplete');
-    boughtAutoComplete();
+	game.buyAutoComplete();       //  boughtAutoComplete();
   });
   
   //    BUTTON: Code generator
     $("#codegenerator").click( function() {
     console.log('Bought code generator');
-	boughtCodeGenerator();
-  });
+	game.buyCodeGenerator();       //  boughtCodeGenerator();
+	if(!codeGenIntervalID){
+        codeGenIntervalID = setInterval(botCode, 1000);
+        console.log("Interval set");
+    } else {
+        console.log("clearing interval and setting a new one");
+        clearInterval(codeGenIntervalID);
+        codeGenIntervalID = setInterval(botCode, 1000/game.getCodeGeneratorLines());
+        console.log("interval set at: " + 1000/game.getCodeGeneratorLines());
+    }});
+  
+function botCode(){
+    $('#code').append(addCode(1));
+    updateScroll();
+}
 
   //	SEND code lines to server
-  var lastLines = lines;
+  var lastLines = game.getLines();
   function sendLines() {
+    var lines = game.getLines();
     socket.emit('sendLines', (lines - lastLines));
     updateBarChart((lines - lastLines));
     lastLines = lines;
@@ -38,3 +57,9 @@
     $("#lines_total").empty();
     $("#lines_total").append(a);
   }
+  
+  $( document ).keydown( function (event) { 
+    console.log("button pressed");
+    $('#code').append(playerCode());
+    updateScroll();
+  });
